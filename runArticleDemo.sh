e2e5ad9140fbd5354fbb60ae073c0ff2b1b4b224
Scott Aaron <dr_saaron@yahoo.com>
2026-01-27 14:14:30 -0600

    ,

--- runArticleDemo.sh
+++ runArticleDemo.sh
@@ -3,18 +3,27 @@
 baseUrl=http://localhost:8080
 workFile=/tmp/tasks.lst
 
-# submit an article
+# submit 2 articles
 curl -X POST -H 'Content-Type: application/json' -d '{ "author": "Scott", "url": "http://home.me" }' $baseUrl/submit
+curl -X POST -H 'Content-Type: application/json' -d '{ "author": "Scott", "url": "http://home2.me" }' $baseUrl/submit
 
 # get tasks
 curl $baseUrl/tasks?assignee=editors > $workFile
 echo "tasks: "
 jq < $workFile
 
-# submit a review for the first one
+# submit an approval for the first one
 id=$(jq '.[0].id' $workFile)
 echo "id = $id"
 curl -X POST -H 'Content-Type: application/json' -d "{ \"id\": $id, \"status\": true }" $baseUrl/review
 
 # get tasks now, the first one should be done
+curl $baseUrl/tasks?assignee=editors | jq
+
+# submit a rejection for the second one
+id=$(jq '.[1].id' $workFile)
+echo "id = $id"
+curl -X POST -H 'Content-Type: application/json' -d "{ \"id\": $id, \"status\": false }" $baseUrl/review
+
+# get tasks now, and both should be done
 curl $baseUrl/tasks?assignee=editors | jq
